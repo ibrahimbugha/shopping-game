5d7ed8347a063a706171474583bc1780d516f1d3
Nour Edin <nouredin.alhabal@gmail.com>
2024-12-06 14:12:57 +0200

    Code clean

--- game/practice.js
+++ game/practice.js
@@ -1,135 +1,132 @@
-var mone= localStorage.getItem("score456566")
-let c= Number(localStorage.getItem("score456566"))
-console.log(c)
-    
+var userMoney = Number(localStorage.getItem("score456566"))
 
 console.log("your money");
-var m = 0
-console.log(m);
+
+console.log(userMoney);
 
 function first(){
-    c = m+=1
-    console.log(c)
-    document.getElementById('txt').innerHTML = c
+    userMoney+=1
+    console.log(userMoney)
+    document.getElementById('txt').innerHTML = userMoney
     document.getElementById('audio').src = 'MONEY SOUND EFFECT !!.mp4'
     return ch ()
 }
 function two(){
-    c+= m+=2
-    console.log(c)
-    document.getElementById('txt').innerHTML = c
+    userMoney+=2
+    console.log(userMoney)
+    document.getElementById('txt').innerHTML = userMoney
     document.getElementById('audio').src = 'MONEY SOUND EFFECT !!.mp4'
     return ch ()
 }
 function three(){
-    c += m+=3
-    console.log(c)
-    document.getElementById('txt').innerHTML = c
+    userMoney+=3
+    console.log(userMoney)
+    document.getElementById('txt').innerHTML = userMoney
     document.getElementById('audio').src = 'MONEY SOUND EFFECT !!.mp4'
     return ch ()
 }
 function four(){
-    c += m+=4
-    console.log(c)
-    document.getElementById('txt').innerHTML = c
+    userMoney+=4
+    console.log(userMoney)
+    document.getElementById('txt').innerHTML = userMoney
     document.getElementById('audio').src = 'MONEY SOUND EFFECT !!.mp4'
     return ch ()
 }
 function five(){
-    c += m+=5
-    console.log(c)
-    document.getElementById('txt').innerHTML = c
+    userMoney+=5
+    console.log(userMoney)
+    document.getElementById('txt').innerHTML = userMoney
     document.getElementById('audio').src = 'MONEY SOUND EFFECT !!.mp4'
     return ch ()
 }
 function six(){
-    c = m+=6
-    console.log(c)
-    document.getElementById('txt').innerHTML = c
+    userMoney+=6
+    console.log(userMoney)
+    document.getElementById('txt').innerHTML = userMoney
     document.getElementById('audio').src = 'MONEY SOUND EFFECT !!.mp4'
     return ch ()
 }
 function seven(){
-    c = m+=7
-    console.log(c)
-    document.getElementById('txt').innerHTML = c
+    userMoney+=7
+    console.log(userMoney)
+    document.getElementById('txt').innerHTML = userMoney
     document.getElementById('audio').src = 'MONEY SOUND EFFECT !!.mp4'
     return ch ()
 }
 function eight(){
-    c = m+=8
-    console.log(c)
-    document.getElementById('txt').innerHTML = c
+    userMoney+=8
+    console.log(userMoney)
+    document.getElementById('txt').innerHTML = userMoney
     document.getElementById('audio').src = 'MONEY SOUND EFFECT !!.mp4'
     return ch ()
 }
 function nine(){
-    c = m+=9
-    console.log(c)
-    document.getElementById('txt').innerHTML = c
+    userMoney+=9
+    console.log(userMoney)
+    document.getElementById('txt').innerHTML = userMoney
     document.getElementById('audio').src = 'MONEY SOUND EFFECT !!.mp4'
     return ch ()
 }
 function ten(){
-    c = m+=10
-    console.log(c)
-    document.getElementById('txt').innerHTML = c
+    userMoney+=10
+    console.log(userMoney)
+    document.getElementById('txt').innerHTML = userMoney
     document.getElementById('audio').src = 'MONEY SOUND EFFECT !!.mp4'
     return ch ()
 }
 function zero(){
-    c = m=0
-    console.log(c)
-    document.getElementById('txt').innerHTML = c
+    userMoney=0
+    console.log(userMoney)
+    document.getElementById('txt').innerHTML = userMoney
     document.getElementById('audio').src = 'sound effect __ fail joke sound effect, funny sound effect.mp3'
     return ch ()
 }
 var xe =0
 function double(){
-    c = m*=2
-    console.log(c)
-    document.getElementById('txt').innerHTML = c
+    userMoney*=2
+    console.log(userMoney)
+    document.getElementById('txt').innerHTML = userMoney
     document.getElementById('audio').src = 'Victory Sound Effect.mp4'
     return ch ()
 }
 function dob() {
     var xe = Number(document.getElementById("xe").value)
 
-var txt = c =m*=xe
-console.log(c)
-    document.getElementById('txt').innerHTML = c
+var txt = userMoney*=xe
+console.log(userMoney)
+    document.getElementById('txt').innerHTML = userMoney
     document.getElementById('audio').src = 'Victory Sound Effect.mp4'
     return ch ()
 }
 function div() {
     var di = Number(document.getElementById("di").value)
 
-var txt = c =m/=di
-console.log(c)
-    document.getElementById('txt').innerHTML = c
+var txt = userMoney/=di
+console.log(userMoney)
+    document.getElementById('txt').innerHTML = userMoney
     document.getElementById('audio').src = 'sound effect __ fail joke sound effect, funny sound effect.mp3'
     return ch ()
 }
 function pls() {
     var pl = Number(document.getElementById("pl").value)
 
-var txt = c =m+=pl
-console.log(c)
-    document.getElementById('txt').innerHTML = c
+var txt = userMoney+=pl
+console.log(userMoney)
+    document.getElementById('txt').innerHTML = userMoney
     document.getElementById('audio').src = 'MONEY SOUND EFFECT !!.mp4'
     return ch ()
 }
 function dec() {
     var de = Number(document.getElementById("de").value)
 
-var txt = c =m-=de
-console.log(c)
-    document.getElementById('txt').innerHTML = c
+var txt = userMoney-=de
+console.log(userMoney)
+    document.getElementById('txt').innerHTML = userMoney
     document.getElementById('audio').src = 'Spongebob Sad - Sound Effect(HD).mp4'
      ch ()
 }
 
 function ch () {
 
-    localStorage.setItem('score456566',c)
+    localStorage.setItem('score456566',userMoney)
 }
